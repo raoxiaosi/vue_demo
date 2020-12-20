950c1e58b3d87a0de502691a87e169ae590cb235
raojing <1770561005@qq.com>
2020-12-20 18:12:14 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
+import { Button, Select, Option } from 'element-ui'
 import App from "./App"
+
+Vue.component(Button.name, Button)
+Vue.component(Select.name, Select)
+Vue.component(Option.name, Option)
 
 new Vue({
   el: '#app',
